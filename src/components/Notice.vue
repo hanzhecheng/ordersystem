72524feede633957aa676541b78cc226d61ebc50
hanzhecheng <1119216543@qq.com>
2019-06-01 17:07:34 +0800

    增加数量组件，增加通告组件

---
+++ src/components/Notice.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class='notice'>
+    <div class='notice-main'>
+      <p>商家公告</p>
+      <div class='notice-main-content'>{{info.notice}}</div>
+    </div>
+    <div class='notice-deliveryfee'>
+      <h5>配送说明</h5>
+      <label>配送费：&yen;{{info.deliveryfee}}</label>
+    </div>
+    <div class='notice-invoice'>
+      <p>票</p>
+      <label>该商家支持开发票，请在下单时填写好发票抬头</label>
+    </div>
+    <div class='notice-report'>举报商家</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notice",
+  props: {
+    info: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.notice {
+  width: 100%;
+}
+.notice-main {
+  background-color: white;
+  p {
+    margin: 0;
+    height: 30px;
+    line-height: 30px;
+    background-color: #0089dc;
+    padding: 10px;
+    color: #fff;
+  }
+}
+.notice-main-content {
+  padding: 10px;
+}
+.notice-deliveryfee {
+  padding: 10px;
+  h5 {
+    margin: 5px 0;
+  }
+  label {
+    font-size: 13px;
+  }
+}
+.notice-invoice {
+  background-color: #fff;
+  display: flex;
+  border-bottom: 1px solid #eee;
+  padding: 10px;
+  p {
+    margin: 0;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    text-align: center;
+    border-radius: 50%;
+    background-color: rgb(153, 153, 153);
+    color: #fff;
+    margin-right: 5px;
+    font-size: 11px;
+  }
+  label {
+    flex: 1;
+    font-size: 13px;
+    color: black;
+  }
+}
+.notice-report {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  color: #666;
+  font-size: 18px;
+  background-color: #fff;
+}
+</style>
+
---
+++ src/components/Quantity.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class='quantity'>
+    <button type='button' class='quantity-btn-minus' @click='minus'>-</button>
+    <input type='text' class='quantity-input' v-model='num' @input='checkValid($event)'>
+    <button type='button' class='quantity-btn-plus' @click='plus'>+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quantity",
+  data() {
+    return {
+      num: 1
+    };
+  },
+  methods: {
+    checkValid(event) {
+      let numReg = /[0-9]/;
+      if (event.data === null) {
+        this.num = this.num === "" ? "" : parseInt(this.num);
+      } else if (!numReg.test(event.data)) {
+        let numS = this.num.toString();
+        this.num = parseInt(numS.substring(0, numS.length - 1));
+      } else {
+        this.num = parseInt(this.num);
+      }
+    },
+    minus() {
+      if (this.num > 1) {
+        this.num -= 1;
+      } else {
+          
+      }
+    },
+    plus() {
+      this.num += 1;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@mixin btn {
+  border: 0;
+  width: 25px;
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
+  height: 22px;
+  line-height: 22px;
+  color: black;
+  outline: 0;
+  cursor: pointer;
+  position: absolute;
+  background-color: rgb(238, 238, 238);
+}
+.quantity {
+  width: 82px;
+  position: relative;
+  border-radius: 10px;
+  height: 22px;
+}
+.quantity-btn-minus {
+  @include btn;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border-right: 1px solid #ddd;
+  left: 0;
+}
+.quantity-input {
+  display: inline-block;
+  border: 0;
+  width: 32px;
+  height: 18px;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  outline: 0;
+  font-size: 12px;
+  text-align: center;
+  position: absolute;
+  left: 25px;
+}
+.quantity-btn-plus {
+  @include btn;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-left: 1px solid #ddd;
+  right: 0;
+}
+</style>
+
