efb1f873b9f42b7b88901d22f96e1f2486a239e5
hanzhecheng <1119216543@qq.com>
2019-06-11 09:51:19 +0800

    :sparkles: 增加打包gzip,增加ora，chalk等引用

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const CompressionWebpackPlugin = require("compression-webpack-plugin")
+const productionGzipExtensions = ['js', 'css']
 module.exports = {
+    build: {
+        productionSourceMap: false
+    },
     chainWebpack: config => {
         //配置svg使用方式
         const svgRule = config.module.rule('svg')
@@ -9,6 +14,14 @@ module.exports = {
             })
         //移除prefetch插件
         config.plugins.delete('prefetch')
-
     },
+    configureWebpack: config => {
+        config.plugins.push(
+            new CompressionWebpackPlugin({
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                threshold: 8192,
+                minRatio: 0.8
+            })
+        );
+    }
 }
