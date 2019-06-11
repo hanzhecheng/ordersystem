ad697cd2272b3dff812b343f6abe7ecff6cbf696
hanzhecheng <1119216543@qq.com>
2019-06-11 11:19:14 +0800

    :sparkles: 修改分类link,增加滚动设置

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -21,12 +21,14 @@ export default {
       currentIndex: 0,
       list: [
         { name: "热销", href: "1" },
-        { name: "优惠", href: "2" },
-        { name: "当季新品", href: "3" },
-        { name: "温馨提示", href: "4" },
-        { name: "招牌柠檬系列", href: "5" },
-        { name: "招牌奶茶系列", href: "6" },
-        { name: "港式小食", href: "7" }
+        { name: "优惠", href: "7" },
+        { name: "当季新品", href: "13" },
+        { name: "温馨提示", href: "19" },
+        { name: "招牌柠檬系列", href: "25" },
+        { name: "招牌奶茶系列", href: "31" },
+        { name: "港式小食", href: "37" },
+        { name: "中式", href: "43" },
+        { name: "炒饭", href: "49" }
       ]
     };
   },
@@ -45,9 +47,15 @@ export default {
   methods: {
     scrollTo(index) {
       this.currentIndex = index;
-      let element = document.getElementById(`goods${this.list[index].href}`);
+      let element = document.getElementById(
+        `body-goodslist-link${this.list[index].href}`
+      );
       if (element) {
+        this.$store.dispatch("updateScroll", true);
         element.scrollIntoView({ behavior: "smooth" });
+        setTimeout(() => {
+          this.$store.dispatch("updateScroll", false);
+        }, 200);
       }
     }
   }
@@ -66,13 +74,13 @@ export default {
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  cursor: pointer;
 }
 .category-item.active {
   color: #0089dc;
 }
 .category-item-name {
   font-size: 12px;
+  cursor: pointer;
 }
 .category-item-dot {
   display: inline-block;
