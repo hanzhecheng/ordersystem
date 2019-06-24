8f59fc49138616ee0e3da588fc49fb4cafd706a9
hanzhecheng <1119216543@qq.com>
2019-06-24 15:30:57 +0800

    增加日期控件，后期用于小程序日历签到使用

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,14 @@ Vue.use(Router);
 const _import = require('./_import')
 export default new Router({
     routes: [{
-        path: "/",
-        name: "home",
-        component: _import("Home/Index")
-    }]
+            path: "/",
+            name: "home",
+            component: _import("Home/Index")
+        },
+        {
+            path: "/table",
+            name: "table",
+            component: _import("Date/Index")
+        }
+    ]
 });
